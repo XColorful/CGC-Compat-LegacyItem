3305fe5e96d1d5aee4589ea60a3b2a398e8ede96
XiaoColorful <XColor_ful@outlook.com>
2026-09-18 15:17:41 +0800

    Correct item registry

--- core/src/main/java/dev/xcolorful/cgccompat/legacyitem/core/init/registry/ModItems.java
+++ core/src/main/java/dev/xcolorful/cgccompat/legacyitem/core/init/registry/ModItems.java
@@ -2,10 +2,16 @@ package dev.xcolorful.cgccompat.legacyitem.core.init.registry;
 
 import dev.xcolorful.cgccompat.legacyitem.CgccLegacyItem;
 import dev.xcolorful.cgccompat.legacyitem.core.api.minecraft.item.LegacyItemType;
+import dev.xcolorful.cgccompat.legacyitem.core.item.ammo.LegacyAmmoItem;
+import dev.xcolorful.cgccompat.legacyitem.core.item.ammobox.LegacyAmmoBoxItem;
+import dev.xcolorful.cgccompat.legacyitem.core.item.attachment.LegacyAttachmentItem;
 import dev.xcolorful.cgccompat.legacyitem.core.item.gun.LegacyGunItem;
 import dev.xcolorful.customgun.CustomGun;
 import dev.xcolorful.customgun.core.api.init.registry.IRegistrar;
 import dev.xcolorful.customgun.core.api.init.registry.IRegistryObject;
+import dev.xcolorful.customgun.core.item.ammo.AmmoItem;
+import dev.xcolorful.customgun.core.item.ammobox.AmmoBoxItem;
+import dev.xcolorful.customgun.core.item.attachment.AttachmentItem;
 import dev.xcolorful.customgun.core.item.gun.GunItem;
 import net.minecraft.world.item.Item;
 
@@ -14,7 +20,7 @@ public class ModItems {
 
 
     public static final IRegistryObject<GunItem> GUN = ITEMS.register(LegacyItemType.GUN.getTagName(), LegacyGunItem::new);
-    public static final IRegistryObject<GunItem> ATTACHMENT = ITEMS.register(LegacyItemType.ATTACHMENT.getTagName(), LegacyGunItem::new);
-    public static final IRegistryObject<GunItem> AMMO = ITEMS.register(LegacyItemType.AMMO.getTagName(), LegacyGunItem::new);
-    public static final IRegistryObject<GunItem> AMMO_BOX = ITEMS.register(LegacyItemType.AMMO_BOX.getTagName(), LegacyGunItem::new);
+    public static final IRegistryObject<AttachmentItem> ATTACHMENT = ITEMS.register(LegacyItemType.ATTACHMENT.getTagName(), LegacyAttachmentItem::new);
+    public static final IRegistryObject<AmmoItem> AMMO = ITEMS.register(LegacyItemType.AMMO.getTagName(), LegacyAmmoItem::new);
+    public static final IRegistryObject<AmmoBoxItem> AMMO_BOX = ITEMS.register(LegacyItemType.AMMO_BOX.getTagName(), LegacyAmmoBoxItem::new);
 }
